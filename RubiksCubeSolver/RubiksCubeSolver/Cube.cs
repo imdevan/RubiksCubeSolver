931650da6b9ed6d6533c9a4384c605ada3ac8b67
Devan Huapaya <huapayadevan@gmail.com>
2014-04-22 16:38:00 -0500

    commiting to merge changes.

--- RubiksCubeSolver/RubiksCubeSolver/Cube.cs
+++ RubiksCubeSolver/RubiksCubeSolver/Cube.cs
@@ -17,133 +17,33 @@ using System.Threading.Tasks;
      * Devan Huapaya
      * 
      * Reference:
+     * source:
      * http://www.codeproject.com/Articles/34362/Rubik-s-Cube-in-a-C-Console-Program
      * *********************************************************************************************************************************************/
 
 namespace RubiksCubeSolver
 {
 
-    /* Face Layout
-    /**********************************************************************************************************************************************/
+    enum Color { RED, BLUE, WHITE, GREEN, YELLOW, ORANGE}
+    enum FaceIndex {FRONT, BACK, RIGHT, UP, LEFT, RIGHT}
 
-    // Specify the Surrounding neighbors of each Face.
-    // This is the description of 2D layout of a Cube.
-    class FaceLayout2D
+    class Cube
     {
-        public sbyte self, right, up, left, down;
-        public FaceLayout2D(sbyte s, sbyte r, sbyte u, sbyte l, sbyte d)
-        {
-            self = s; 
-            right = r;
-            up = u;
-            left = l;
-            down = d;
-        }
-        
-        public static Dictionary<sbyte, FaceLayout2D> layout = new Dictionary<sbyte, FaceLayout2D>();
-        static FaceLayout2D() {
-            
-        }
+        Color color;
+        Color [] adjacentColors; // size: 2-4
+
+        sbyte index;
+        sbyte [,,] position; // face, y, x
     }
 
-
-    class CubiePosition
-    {
-        /*
-         * for Position:
-         * 1. When a1 = a2 = a3, the cubie is center
-         * 2. when a1 == a2 < a3, the cubie is no an edge
-         * 3. when a1<a2<a3, the cubie is on a corner
-         */
-        public sbyte a1, a2, a3;
-
-        public CubiePosition(sbyte b1, sbyte b2, sbyte b3)
-        {
-            a1 = b1; 
-            a2 = b2;
-            a3 = b3;
-            AdjustOrder();
-        }
-
-        private void AdjustOrder(){
-            // a1 <= a2 <= a3
-            sbyte b1 = a1;
-            sbyte b2 = a2;
-            sbyte b3 = a3;
-            a1 = Math.Min(b1, Math.Min(b2,b3));
-            a3 = Math.Max(b1, Math.Max(b2,b3));
-            a2 = (sbyte)(b1 + b2 + b3 - a1 - a3); // middle
-            if(a2 == a3)
-            {
-                // only keep the smaller
-                a2 = a1;
-            }
-        }
-
-        public override string ToString(){
-            return String.Format("{0}, {1}, {2})", a1, a2, a3);
-        }
-        public CubiePosition Clone() 
-        {
-            return new CubiePosition(a1, a2, a3);
-        }
-
-        public int CompareTo(CubiePosition p){
-            if(p.a1 != a1){
-                return a1 - p.a1;
-            }
-            if(p.a2 != a2){
-                return a2 - p.a2;
-            }
-            return a3 - p.a3;
-        }
-
-        public bool IsCenter(){
-            return a1 == a2 && a2 == a3;
-        }
-
-        public bool IsEdge(){
-            return (a1 == a2 || a2 == a3 );
-        }
-        
-        public bool IsCorner(){
-            return a1 < a2 && a2 < a3;
-        }
-
-        public void RatateFrontClockwise90Degrees(){
-            //a1 = Face.RotateFrontClockwise90Degree(a1);
-            //a2 = Face.RotateFrontClockwise90Degree(a2);
-            //a3 = Face.RotateFrontClockwise90Degree(a3);
-        } 
-
-        public void MapFrontFrom(sbyte side)
-        {
-
-        }
-
-    }
-
-    /* Rubiks Cube Object Definitions
-    /**********************************************************************************************************************************************/
     class Face
     {
-        /*
-         * A cube has six faces
-         * 0 F = front face
-         * 1 R = right face
-         * 2 U = up face
-         * 3 L = left face
-         * 4 D = down face
-         * 5 B = back face
-         * */
-        public const sbyte Front = 0;
-        public const sbyte Right = 1;
-        public const sbyte Up = 2;
-        public const sbyte Left = 3;
-        public const sbyte Down = 4;
-        public const sbyte Back = 5;
-
-        public static readonly Dictionary<int, string> FaceNames = new Dictionary<int,string>(); // { "Front", "Right", "Up", "Left", "Down", "Back" };
-
+        FaceIndex index;
+        Cube[,] cubies;
     }
+    class Cube
+    {
+        Face[] faces;
+    }
+   
 }
